b5da54a3d049c53ad885e5eaf9081b43a1c725df
josRosario <jrosario@intellisysdcorp.com>
2024-08-26 12:50:52 -0400

    update address

--- src/service/address.service.js
+++ src/service/address.service.js
@@ -7,3 +7,19 @@ export const createAddress = async (address_data) => {
         throw new Error("Hubo un error al agregar la direccion", error.message)
     }
 }
+
+
+
+export const updateAddress = async (id, address_data) => {
+    try {
+        const address = await Address.findByPk(id);
+
+        if (!address){
+            throw Error('Direccion no encontrada');
+        }
+        return await Address.update(address_data);
+    } catch (error) {
+        throw Error("Hubo un error al actualizar los datos u obtener la direccion", error);
+
+    }
+}
